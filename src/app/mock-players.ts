a4a84cbc02f81541a230ef3ed22ccd257471a928
dfili8 <dfilimon.s@gmail.com>
2019-04-27 18:51:41 -0400

    Master/Details components

---
+++ src/app/mock-players.ts
@@ -0,0 +1,14 @@
+import { Player } from './player';
+
+export const PLAYERS: Player[] = [
+    { number: 24, name: 'Kobe Byrant', position: 'Shooting Guard' },
+    { number: 23, name: 'Michael Jordan', position: 'Shooting Guard' },
+    { number: 30, name: 'Steph Curry', position: 'Point Guard' },
+    { number: 32, name: 'Shaq ONeil', position: 'Center' },
+    { number: 3, name: 'Ben Wallace', position: 'Center' },
+    { number: 1, name: 'Tracy McGrady', position: 'Shooting Guard' },
+    { number: 21, name: 'Tim Duncan', position: 'Power Forward' },
+    { number: 3, name: 'Allen Iverson', position: 'Point Guard' },
+    { number: 71, name: 'Luka Doncic', position: 'Shooting Guard' },
+    { number: 41, name: 'Dirk Nowitski', position: 'Small Forward' }
+];
---
+++ src/app/player.ts
@@ -0,0 +1,5 @@
+export class Player {
+    number: number;
+    name: string;
+    position: string;
+}
